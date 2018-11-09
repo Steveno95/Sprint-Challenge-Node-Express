54fe1a7ea7a6cb0d484e20e4f3e520e706e4feaf
Steven O'Neill <oneillsteven957@gmail.com>
2018-11-09 11:45:42 -0600

    Got post, put, and delete requests for actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -34,6 +34,49 @@ router.get('/', (req, res) => {
       });
   });
   
- 
+  router.post('/', async (req, res) => {
+    console.log('body', req.body);
+    try {
+      const actionData = req.body;
+      const actionId = await action.insert(actionData);
+      const actions = await action.get(actionId.id);
+      res.status(201).json(actions);
+    } catch (error) {
+      let message = 'error creating the action';
+  
+      if (error.errno === 19) {
+        message = 'please provide a project id, a description, notes, and if it is completed';
+      }
+  
+      res.status(500).json({ message, error });
+    }
+  });
+
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    action.update(id, changes)
+    .then(actionUpdate => {
+      if (actionUpdate) {
+        res.status(200).json({ message: 'action updated' });
+      } else {
+        res.status(404).json({ message: 'action not found' })
+      }
+      
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'error updating the action', err });
+    })
+  });
+  
+  router.delete('/:id', (req, res) => {
+    action.remove(req.params.id)
+      .then(count => {
+        res.status(200).json(count);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'error deleting action', err });
+      });
+  });
 
 module.exports = router;
